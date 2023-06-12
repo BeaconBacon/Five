2d11dcae168f7d89ff0594424bfb75472bcfcf41
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2023-06-12 17:24:23 -0500

    finished up to the homepage

---
+++ Build-an-Animated-App-with-Rive-and-Flutter-Starting/lib/entry_point.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+import 'package:rive_animation/constants.dart';
+import 'package:rive_animation/screens/home/home_screen.dart';
+import 'package:rive_animation/screens/onboding/components/animated_bar.dart';
+import 'package:rive_animation/utils/rive_utils.dart';
+
+import 'models/rive_asset.dart';
+
+class EntryPoint extends StatefulWidget {
+  const EntryPoint({super.key});
+
+  @override
+  State<EntryPoint> createState() => _EntryPointState();
+}
+
+class _EntryPointState extends State<EntryPoint> {
+  RiveAssets selectedBottomNav = bottomNavs.first;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      extendBody: true,
+      body: const HomeScreen(),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
+          decoration: BoxDecoration(
+            color: backgroundColorDark.withOpacity(0.8),
+            borderRadius: const BorderRadius.all(Radius.circular(24)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              ...List.generate(
+                bottomNavs.length,
+                (index) => GestureDetector(
+                  onTap: () {
+                    bottomNavs[index].input!.change(true);
+                    if (bottomNavs[index] != selectedBottomNav) {
+                      setState(() {
+                        selectedBottomNav = bottomNavs[index];
+                      });
+                    }
+                    Future.delayed(const Duration(seconds: 1), () {
+                      bottomNavs[index].input!.change(false);
+                    });
+                  },
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      // Add animation to it
+                      AnimatedBar(
+                          isActive: bottomNavs[index] == selectedBottomNav),
+                      SizedBox(
+                        height: 36,
+                        width: 36,
+                        child: Opacity(
+                          opacity:
+                              bottomNavs[index] == selectedBottomNav ? 1 : 0.5,
+                          child: RiveAnimation.asset(
+                            // Scr is same for all
+                            bottomNavs.first.src,
+                            artboard: bottomNavs[index].artboard,
+                            onInit: (artboard) {
+                              StateMachineController controller =
+                                  RiveUtils.getRiveController(artboard,
+                                      stateMachineName:
+                                          bottomNavs[index].stateMachineName);
+                              bottomNavs[index].input =
+                                  controller.findSMI("active") as SMIBool;
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
